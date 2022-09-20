8c3199fba8ab219525aa0f750b8aad044779480d
gurkantngl <gurkantngl@gmail.com>
2022-09-20 13:00:56 +0300

    miniProje

---
+++ sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,21 @@
+package sesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char harf = 'E';
+		
+		switch(harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalin sesli harf");
+			break;
+		default:
+			System.out.println("İnce sesli harf");
+		}
+
+	}
+
+}
