1a5893370010bfc5afe24ab1bafab860337c2a03
gurkantngl <gurkantngl@gmail.com>
2022-09-20 12:35:53 +0300

    variables

---
+++ variables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int ogrenciSayisi = 12;
+        String mesaj = "Ogrenci sayisi: ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Ogrenci sayisi: " + ogrenciSayisi);
+        System.out.println("Ogrenci sayisi: " + ogrenciSayisi);
+        System.out.println("Ogrenci sayisi: " + ogrenciSayisi);
+
+
+    }
+}
