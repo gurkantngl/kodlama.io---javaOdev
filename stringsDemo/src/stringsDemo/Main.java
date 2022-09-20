988e0e758006c9ef1922e7d223f41be60f80da89
gurkantngl <gurkantngl@gmail.com>
2022-09-20 13:04:23 +0300

    javaStrings

---
+++ stringsDemo/src/stringsDemo/Main.java
@@ -0,0 +1,30 @@
+package stringsDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String mesaj = "    Bugun hava cok guzel.       ";
+		/*
+		System.out.println(mesaj);
+		System.out.println("Eleman sayisi: " + mesaj.length());
+		System.out.println("5.eleman: " + mesaj.charAt(4));
+		System.out.println(mesaj.concat(" Yasasin"));
+		System.out.println(mesaj.startsWith("B"));
+		System.out.println(mesaj.endsWith("."));
+		char[] characters  = new char[5];
+		mesaj.getChars(0,5,characters,0);
+		System.out.println(characters);
+		System.out.println(mesaj.indexOf('a'));
+		System.out.println(mesaj.lastIndexOf('a'));*/
+		
+		System.out.println(mesaj.replace(' ', '-'));
+		System.out.println(mesaj.substring(2,5));
+		for(String kelime : mesaj.split(" ")) {
+			System.out.println(kelime);
+		}
+		System.out.println(mesaj.toLowerCase());
+		System.out.println(mesaj.toUpperCase());
+		System.out.println(mesaj.trim());
+	}
+
+}
